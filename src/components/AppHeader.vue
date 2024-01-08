5fccada99bf7e9dbea2e99acd59ba75f6a1575e8
UmbertoSerino <serumberto@gmail.com>
2024-01-08 13:03:31 +0100

    Created appVue, and component AppHeader, AppNavbar, install bootstrap and added color palette

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+import AppNavbar from './AppNavbar.vue';
+
+export default {
+  name: 'AppHeader',
+  data() {
+    return {};
+  },
+  components: { AppNavbar }
+}
+</script>
+
+<template>
+  <AppNavbar />
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/mixins' as*;
+@use '../style/partials/variables' as*;
+</style>
