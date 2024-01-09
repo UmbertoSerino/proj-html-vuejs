911f3024759c961259dc5777c08625e8a572cc8a
UmbertoSerino <serumberto@gmail.com>
2024-01-10 00:43:36 +0100

    created section footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import AppHeader from './components/Header/AppHeader.vue';
 import AppMain from './components/Main/AppMain.vue';
+import AppFooter from './components/Footer/AppFooter.vue';
 </script>
 
 <template>
   <AppHeader />
   <AppMain />
+  <AppFooter />
 </template>
 
 <style lang="scss">
---
+++ src/components/Footer/AppFooter.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+    name: 'AppFooter',
+    data() {
+        return {
+            Address: [
+                { street: '382 NE 191st St # 87394 Miami, FL 33179-3899', phoneNumber: '+1(305)547-9909', workTime: '9am - 5pm EST, Monday - Friday', mail: 'support@maxcoach.com' }
+            ],
+            explore: ['Start here', 'Blog', 'About us', 'Success story', 'Courses', 'Contact us'],
+            information: ['Membership', 'Purchase guide', 'Privacy policy', 'Terms of service']
+        }
+    },
+}
+</script>
+<template>
+    <footer class="">
+        <section class="my_wrapper">
+            <div class="row">
+                <div class="col-6" v-for="info in Address" :key="info">
+                    <h1>Address</h1>
+                    <p>
+                        {{ info.street }}
+                    </p>
+                    <p>
+                        {{ info.phoneNumber }} ( {{ info.workTime }} )
+                    </p>
+                    <p>
+                        {{ info.mail }}
+                    </p>
+                </div>
+            </div>
+        </section>
+    </footer>
+</template>
+<style lang="scss">
+@use '../../style/partials/mixins' as*;
+@use '../../style/partials/variables' as*;
+
+footer {
+    background-color: $color-9;
+    height: 500px;
+
+    section.my_wrapper {
+        @include container (width, margin);
+    }
+}
+</style>
